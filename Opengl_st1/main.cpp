3ad7fc2167de415659a9391e3b79f61f7fa7b9d1
tangwen <18210436456@163.com>
2019-08-11 12:46:17 +0800

    Initial Commit

---
+++ Opengl_st1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Opengl_st1
+//
+//  Created by 汤文 on 2019/8/11.
+//  Copyright © 2019 汤文. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
