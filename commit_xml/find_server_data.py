146f0318405cfb3c738d1c8b5cfcda4e873ea18c
tangwen <inryygy007@gamil.com>
2020-09-09 00:06:56 +0800

    获取服务器数据data1

---
+++ commit_xml/find_server_data.py
@@ -0,0 +1,148 @@
+import os, sys, shutil, re
+import subprocess, json
+
+get1_re = re.compile(r'ftc.ManagerData.get1\("(.*?)"\)')
+
+def is_wanted_object(symbol_name,sb_object):
+    if type(sb_object) is dict:
+        if sb_object.get("type") == "MemberExpression":
+            if sb_object.get("object") and sb_object.get("property"):
+                if sb_object["object"]["type"] == "Identifier" and sb_object["object"]["name"] == symbol_name:
+                    return sb_object["property"]["name"]
+    return None
+
+def find_mem_object(symbol_name,mem_map,script_json):
+    if type(script_json) is dict:
+        res = is_wanted_object(symbol_name,script_json)
+        if res:
+            if not res in mem_map:
+                mem_map.append(res)
+        else:
+            for x in script_json:
+                if type(script_json[x]) is dict:
+                    res = is_wanted_object(symbol_name,script_json[x])
+                    if res:
+                        if not res in mem_map:
+                            mem_map.append(res)
+                    else:
+                        find_mem_object(symbol_name,mem_map,script_json[x])
+                elif type(script_json[x]) is list:
+                    for y in script_json[x]:
+                        find_mem_object(symbol_name,mem_map,y)
+
+def fix_bracer(in_str):
+    bracer = []
+    res = []
+    for i,ch in enumerate(in_str):
+        res.append(ch)
+        if ch == '(' or ch == '{' or ch  == '[':
+            bracer.append(ch)
+        elif ch == ')' or ch == '}' or ch  == ']':
+            if len(bracer) == 0:
+                res.pop()
+                continue
+            if bracer[-1] == '(' and ch == ')':
+                bracer.pop()
+                continue
+
+            if bracer[-1] == '{' and ch == '}':
+                bracer.pop()
+                continue
+            
+            if bracer[-1] == '[' and ch == ']':
+                bracer.pop()
+                continue
+            res.pop()
+    return ''.join(res)
+
+def find_get1_data(contents,data1):
+    for match in re.finditer(get1_re, contents):
+        sStart = match.start()
+        sEnd = match.end()
+        sGroup = match.group()
+        t_key = match.group(1)
+        data1[t_key] = data1.get(t_key) if data1.get(t_key) else {}
+        key_name_arr = []
+        if contents[sEnd] == ".":
+            for i in range(52):
+                tv = contents[sEnd+1+i]
+                if len(re.findall('\w',tv)) == 0:
+                    break
+                else:
+                    key_name_arr.append(tv)
+            data1[t_key][''.join(key_name_arr)] = ""
+        elif contents[sEnd] == "[":
+            for i in range(52):
+                tv = contents[sEnd+2+i]
+                if len(re.findall('\w',tv)) == 0:
+                    break
+                else:
+                    key_name_arr.append(tv)
+            data1[t_key][''.join(key_name_arr)] = ""
+        else:
+            symbol_name = ""
+            equal_idx = None
+            s_flag = None
+            for i in range(52):
+                tidx = sStart-1-i
+                tv = contents[tidx]
+                if tv == '=':
+                    equal_idx = tidx
+                elif equal_idx and len(re.findall('\w',tv)) == 1:
+                    s_flag = tidx
+                elif equal_idx and s_flag and len(re.findall('\w',tv)) == 0:
+                    break
+            print("temp variable name {}".format(contents[s_flag:equal_idx]))
+            symbol_name = contents[s_flag:equal_idx]
+            if equal_idx and s_flag:
+                brace_arr = []
+                brace_i = 0
+                t_i = sEnd
+                while True:
+                    brace_i += 1
+                    t_i = sEnd + brace_i
+                    if contents[t_i] == '{':
+                        brace_arr.append(contents[t_i])
+                    elif contents[t_i] == '}':
+                        if len(brace_arr) == 0:
+                            break
+                        else:
+                            brace_arr.pop()
+                script_str = contents[sStart:t_i]
+                script_str = fix_bracer(script_str)
+                script_str = 'function wrap(){{{}}}'.format(script_str)
+                try:
+                    script_json_str = subprocess.check_output(['node','jsgrama.js',script_str])
+                except Exception as e:
+                    pass
+                script_json = json.loads(script_json_str)
+                mem_map = []
+                find_mem_object(symbol_name,mem_map,script_json)
+                for mm in mem_map:
+                    data1[t_key][mm] = ""
+            pass
+
+def main():
+    if len(sys.argv) == 2: #gen ftc.ManagerData only use for 三国吞噬无界
+        with open(sys.argv[1],"r") as f:
+            data1 = {}
+            data2 = {}
+            _data2Id = {}
+            _dataKeys = {}
+            contents = f.read()
+            
+            find_get1_data(contents,data1)
+
+
+            ss = get1_re.findall(contents)
+            c1 = get1_re.search(contents)
+            c2 = c1.groups()
+            c3 = c1.group()
+            c4 = c1.groupdict()
+            for x in c1.group():
+                a = x
+                b = a
+            sa = ss
+
+if __name__ == "__main__":
+    main()
