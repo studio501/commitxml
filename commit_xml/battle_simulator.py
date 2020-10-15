a957e1c6cfd2e850bdc7d6b38761bfe684d5508e
tangwen <inryygy007@gamil.com>
2020-10-15 08:06:38 +0800

    add battle simulator

---
+++ commit_xml/battle_simulator.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*
+from __future__ import print_function
+import sys,os,shutil,re,codecs,subprocess
+
+def main():
+    pass
+    dst_dir = "/Users/tangwen/Documents/my_projects/js_project/new_account/web_sockets_guide4_1"
+    
+if __name__ == "__main__":
+    main()
